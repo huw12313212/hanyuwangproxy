535f6c717118d8beb6076d13a181c6c9a2208bfc
王瀚宇 <huw12313212@gmail.com>
2014-07-25 11:34:56 +0800

    message

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ net.createServer(function (socket) {
   // Identify this client
   socket.name = socket.remoteAddress + ":" + socket.remotePort;
 
-  socket.write('{"command":"test"}\n');
+  socket.write("hello world\n");
   console.log("Connected : "+socket.name);
  
   socket.on('data', function (data) {

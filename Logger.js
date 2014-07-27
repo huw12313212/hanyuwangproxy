0b24a86383951bbcd51a1c8886d97fb7a82e3454
王瀚宇 <huw12313212@gmail.com>
2014-07-27 13:12:56 +0800

    log time

--- Logger.js
+++ Logger.js
@@ -2,13 +2,24 @@ var fs = require('fs');
 
 var Logger = {};
 
+Logger.timestamp = false;
+
 module.exports = Logger;
 
 Logger.log = function(str,file)
 {
   console.log(str);
 
-  fs.appendFile(file, str+"\n", function (err) {
+  var data = str;
+
+  if(Logger.timestamp)
+  {
+  	var time = new Date();
+  	data = "["+time.toString()+"]"+data;
+  }
+
+
+  fs.appendFile(file, data+"\n", function (err) {
     if (err) 
       {
         console.log("Logger Error");
